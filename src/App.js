09dbdddd8e1280e6ea956cc69f21440969c0f32c
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-02 12:06:33 -0500

    try layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Button from "antd/es/button";
 import './App.css';
+import Layout from "antd/es/layout";
+
+const { Header, Footer, Sider, Content } = Layout;
 
 
 function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <Button type="primary">Button</Button>
-        Hello
-      </header>
-      
+      <Button type="primary">Button</Button>
+      <Layout>
+        <Sider style={{background:'red'}}>Sider</Sider>
+        <Layout>
+          <Header style={{background:'green'}}>Header</Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </Layout>
     </div>
   );
 }
