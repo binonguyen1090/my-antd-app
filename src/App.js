e0c230b2e2c716e5e793581afa840080df80454e
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-02 16:24:00 -0500

    add title

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from "antd/es/button";
 import './App.css';
 import Layout from "antd/es/layout";
+import Title from 'antd/lib/typography/Title';
 
 const { Header, Footer, Sider, Content } = Layout;
 
@@ -12,11 +13,17 @@ function App() {
     <div className="App">
       <Button type="primary">Button</Button>
       <Layout>
-        <Sider style={{background:'red'}}>Sider</Sider>
+        <Header style={{ background: "green" }}>
+          <Title style={{ color: "white" }} level={3}>
+            Title
+          </Title>
+        </Header>
         <Layout>
-          <Header style={{background:'green'}}>Header</Header>
-          <Content>Content</Content>
-          <Footer>Footer</Footer>
+          <Sider style={{ background: "red" }}>Sider</Sider>
+          <Layout>
+            <Content>Content</Content>
+            <Footer>Footer</Footer>
+          </Layout>
         </Layout>
       </Layout>
     </div>
