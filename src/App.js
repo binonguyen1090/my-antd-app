e7c786eaf738c1a29f77a762a2b61d7216a0581e
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-07 11:21:57 -0500

    add Menu but cant connect children yet

--- src/App.js
+++ src/App.js
@@ -31,39 +31,6 @@ const { TabPane } = Tabs;
 
 
 
-  const moveActions = {
-    go: url => (dispatch) => {
-        return dispatch(push(url));
-    }
-};
-
-connect(
-    null,
-    {...moveActions}
-);
-const { Header, Footer, Sider, Content } = Layout;
-class App extends React.Component {
-  render() {
-    return (
-      <Layout className="rootLayout">
-        <Sider collapsible>
-          <div className="logo">ant-react-redux</div>
-          1
-          {/* <Menus go={this.props.go} router={this.props.router} /> */}
-        </Sider>
-        <Content className="contentParent">
-          <Header className="header">Header</Header>
-          <Content className="content">{this.props.children}</Content>
-        </Content>
-      </Layout>
-    );
-  }
-}
-
-export default App;
-// class App extends Component {
-
-// function App() {
 //   const moveActions = {
 //     go: url => (dispatch) => {
 //         return dispatch(push(url));
@@ -74,240 +41,273 @@ export default App;
 //     null,
 //     {...moveActions}
 // );
-//   // const [stateValue, setStateValue] = useState('')
-//   // const [stateBool, setStateBool] = useState(false)
-//   const [selectedPlayer, setselectedPlayer] = useState("");
-//   const [visible, setvisible] = useState(false);
-//   const onselect = name => {
-//     setselectedPlayer(name)
-//     setvisible(true)
+const { Header, Footer, Sider, Content } = Layout;
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <Layout className="rootLayout">
+//         <Sider collapsible>
+//           <div className="logo">ant-react-redux</div>
+//           1
+//           {/* <Menus go={this.props.go} router={this.props.router} /> */}
+//         </Sider>
+//         <Content className="contentParent">
+//           <Header className="header">Header</Header>
+//           <Content className="content">{this.props.children}</Content>
+//         </Content>
+//       </Layout>
+//     );
 //   }
-//   const ViewProfileButton = ({name}) => {
-//     return <Button type="dashed" style={{float:'right'}} onClick={() => onselect(name)}> View Profile >>> </Button>
-//   }
-//   const onClose = () => {
-//     setvisible(false)
-//   }
-//   const onModal = () => {
-//     Modal.info({
-//       title: "Hello",
-//       content: (
-//         <div>
-//           <p>Congratulations, button works</p>
-//         </div>
-//       ),
-//     });
-//   };
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Layout>
-//           <Header style={{ background: "white" }}>ant-react-redux</Header>
-//         </Layout>
-
-//         <Layout style={{ padding: "10px" }}>
-//           <Sider width={200}>
-//             <Menu
-//               mode="inline"
-//               defaultSelectedKeys={["1"]}
-//               defaultOpenKeys={["sub1"]}
-//               style={{ height: "100%", borderRight: 0 }}
-//             >
-//               <Menu.Item key="sub1" icon={<UserOutlined />}>
-//                 Menu A
-//               </Menu.Item>
-//               <SubMenu
-//                 key="sub2"
-//                 icon={<LaptopOutlined />}
-//                 title="Navigation One"
-//               >
-//                 <Menu.Item key="5">option 1</Menu.Item>
-//                 <Menu.Item key="6">option 2</Menu.Item>
-//               </SubMenu>
-//             </Menu>
-//           </Sider>
-//           <Layout
-//             style={{
-//               padding: "10px 10px 24px 44px",
-//               margin: { top: 0, right: 0, bottom: 0, left: 10 },
-//             }}
-//           >
-//             <Breadcrumb style={{ margin: "16px 0" }}>
-//               <Breadcrumb.Item>
-//                 <HomeOutlined />
-//               </Breadcrumb.Item>
-//               <Breadcrumb.Item>
-//                 <UserOutlined />
-//                 Menu A
-//               </Breadcrumb.Item>
-//               <Breadcrumb.Item>List</Breadcrumb.Item>
-//             </Breadcrumb>
-//             <Content
-//               className="site-layout-background"
-//               style={{
-//                 padding: 24,
-//                 margin: 0,
-//                 minHeight: 280,
-//               }}
-//             >
-//                 <Content className="content">{this.props.children}</Content>
-//             </Content>
-//           </Layout>
-//         </Layout>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <Layout>
-//           <Header style={{ background: "green", padding: "10px" }}>
-//             <Avatar style={{ float: "right" }} icon={<UserOutlined />} />
-
-//             <Title style={{ color: "white" }} level={3}>
-//               Brand
-//             </Title>
-//           </Header>
-//           <Tabs defaultActiveKey="1" style={{ float: "right" }}>
-//             <TabPane tab="Tab 1" key="1">
-//               Content of Tab Pane 1
-//             </TabPane>
-//             <TabPane tab="Tab 2" key="2">
-//               Content of Tab Pane 2
-//             </TabPane>
-//             <TabPane tab="Tab 3" key="3">
-//               Content of Tab Pane 3
-//             </TabPane>
-//           </Tabs>
-//           <Layout>
-//             <Sider style={{ background: "red" }}>
-//               <Menu defaultSelectedKeys={["Dashboard"]} mode="inline">
-//                 <Menu.Item key="Dashboard">Dashboard</Menu.Item>
-//                 <SubMenu
-//                   title={
-//                     <span>
-//                       <MailOutlined />
-//                       <span>About Us</span>
-//                     </span>
-//                   }
-//                 >
-//                   <Menu.ItemGroup key="AboutUs" title="Country">
-//                     <Menu.Item>America</Menu.Item>
-//                     <Menu.Item>England</Menu.Item>
-//                   </Menu.ItemGroup>
-//                 </SubMenu>
-//               </Menu>
-//             </Sider>
-//             <Layout>
-//               <Content style={{ padding: "0 50px" }}>
-//                 <Breadcrumb style={{ margin: "16px 0" }}>
-//                   <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
-//                 </Breadcrumb>
-//                 <div
-//                   style={{ background: "#fff", padding: 24, minHeight: 480 }}
-//                 >
-//                   {/* <Batsmen name="Smith" team="AUS" runs="6973" />
-//                 <Bowler name="Pat" team="AUS" wickets="123" /> */}
-//                   <Cricketer
-//                     name="Virat Kholi"
-//                     team="IND"
-//                     avatarSrc="./cat.png"
-//                   >
-//                     <ODICareer matches="239">
-//                       <Batting runs="11,520" score="183" />
-//                       <br></br>
-//                       <Bowling wickets="4" bowlingAvg="166.25" />
-//                     </ODICareer>
-//                     <TestCareer matches="79">
-//                       <Batting runs="6,749" score="243" />
-//                     </TestCareer>
-//                     <ViewProfileButton name="Virat Kholi" />
-//                   </Cricketer>
-//                   <Cricketer name="J Bumrah" team="IND" avatarSrc="./bb.jpg">
-//                     <TestCareer matches="12">
-//                       <Bowling wickets="62" bowlingAvg="20.63" />
-//                     </TestCareer>
-//                     <ViewProfileButton name="J Bumrah" />
-//                   </Cricketer>
-//                 </div>
-//               </Content>
-//               <CareerDetails
-//                 player={selectedPlayer}
-//                 visible={visible}
-//                 onClose={onClose}
-//               />
-//               <Footer style={{ textAlign: "center" }}>
-//                 Ant Design ©2018 Created by practice
-//               </Footer>
-//             </Layout>
-//           </Layout>
-//         </Layout>
-//         <Button onClick={onModal} type="primary">
-//           Modal Button
-//         </Button>
-//         <Todo />
-//       </div>
-//     </Router>
-//   );
 // }
 
 // export default App;
+// class App extends Component {
+
+function App() {
+  const moveActions = {
+    go: url => (dispatch) => {
+        return dispatch(push(url));
+    }
+};
+
+connect(
+    null,
+    {...moveActions}
+);
+  // const [stateValue, setStateValue] = useState('')
+  // const [stateBool, setStateBool] = useState(false)
+  const [selectedPlayer, setselectedPlayer] = useState("");
+  const [visible, setvisible] = useState(false);
+  const onselect = name => {
+    setselectedPlayer(name)
+    setvisible(true)
+  }
+  const ViewProfileButton = ({name}) => {
+    return <Button type="dashed" style={{float:'right'}} onClick={() => onselect(name)}> View Profile >>> </Button>
+  }
+  const onClose = () => {
+    setvisible(false)
+  }
+  const onModal = () => {
+    Modal.info({
+      title: "Hello",
+      content: (
+        <div>
+          <p>Congratulations, button works</p>
+        </div>
+      ),
+    });
+  };
+  return (
+    <Router>
+      <div className="App">
+        <Layout>
+          <Header style={{ background: "white" }}>ant-react-redux</Header>
+        </Layout>
+
+        <Layout style={{ padding: "10px" }}>
+          <Sider width={200}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={["1"]}
+              defaultOpenKeys={["sub1"]}
+              style={{ height: "100%", borderRight: 0 }}
+            >
+              <Menu.Item key="sub1" icon={<UserOutlined />}>
+                Menu A
+              </Menu.Item>
+              <SubMenu
+                key="sub2"
+                icon={<LaptopOutlined />}
+                title="Navigation One"
+              >
+                <Menu.Item key="5">option 1</Menu.Item>
+                <Menu.Item key="6">option 2</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout
+            style={{
+              padding: "10px 10px 24px 44px",
+              margin: { top: 0, right: 0, bottom: 0, left: 10 },
+            }}
+          >
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>
+                <HomeOutlined />
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <UserOutlined />
+                Menu A
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+              }}
+            >
+                {/* <Content className="content">{this.props.children}</Content> */}
+            </Content>
+          </Layout>
+        </Layout>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <Layout>
+          <Header style={{ background: "green", padding: "10px" }}>
+            <Avatar style={{ float: "right" }} icon={<UserOutlined />} />
+
+            <Title style={{ color: "white" }} level={3}>
+              Brand
+            </Title>
+          </Header>
+          <Tabs defaultActiveKey="1" style={{ float: "right" }}>
+            <TabPane tab="Tab 1" key="1">
+              Content of Tab Pane 1
+            </TabPane>
+            <TabPane tab="Tab 2" key="2">
+              Content of Tab Pane 2
+            </TabPane>
+            <TabPane tab="Tab 3" key="3">
+              Content of Tab Pane 3
+            </TabPane>
+          </Tabs>
+          <Layout>
+            <Sider style={{ background: "red" }}>
+              <Menu defaultSelectedKeys={["Dashboard"]} mode="inline">
+                <Menu.Item key="Dashboard">Dashboard</Menu.Item>
+                <SubMenu
+                  title={
+                    <span>
+                      <MailOutlined />
+                      <span>About Us</span>
+                    </span>
+                  }
+                >
+                  <Menu.ItemGroup key="AboutUs" title="Country">
+                    <Menu.Item>America</Menu.Item>
+                    <Menu.Item>England</Menu.Item>
+                  </Menu.ItemGroup>
+                </SubMenu>
+              </Menu>
+            </Sider>
+            <Layout>
+              <Content style={{ padding: "0 50px" }}>
+                <Breadcrumb style={{ margin: "16px 0" }}>
+                  <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+                </Breadcrumb>
+                <div
+                  style={{ background: "#fff", padding: 24, minHeight: 480 }}
+                >
+                  {/* <Batsmen name="Smith" team="AUS" runs="6973" />
+                <Bowler name="Pat" team="AUS" wickets="123" /> */}
+                  <Cricketer
+                    name="Virat Kholi"
+                    team="IND"
+                    avatarSrc="./cat.png"
+                  >
+                    <ODICareer matches="239">
+                      <Batting runs="11,520" score="183" />
+                      <br></br>
+                      <Bowling wickets="4" bowlingAvg="166.25" />
+                    </ODICareer>
+                    <TestCareer matches="79">
+                      <Batting runs="6,749" score="243" />
+                    </TestCareer>
+                    <ViewProfileButton name="Virat Kholi" />
+                  </Cricketer>
+                  <Cricketer name="J Bumrah" team="IND" avatarSrc="./bb.jpg">
+                    <TestCareer matches="12">
+                      <Bowling wickets="62" bowlingAvg="20.63" />
+                    </TestCareer>
+                    <ViewProfileButton name="J Bumrah" />
+                  </Cricketer>
+                </div>
+              </Content>
+              <CareerDetails
+                player={selectedPlayer}
+                visible={visible}
+                onClose={onClose}
+              />
+              <Footer style={{ textAlign: "center" }}>
+                Ant Design ©2018 Created by practice
+              </Footer>
+            </Layout>
+          </Layout>
+        </Layout>
+        <Button onClick={onModal} type="primary">
+          Modal Button
+        </Button>
+        <Todo />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Menus.js
@@ -0,0 +1,55 @@
+import React, {Component, PropTypes} from "react";
+import Icon from "antd/es/icon";
+import Menu from "antd/es/menu";
+
+const SubMenu = Menu.SubMenu;
+
+class Menus extends Component {
+    constructor() {
+        super();
+        this.state = {
+            selectedKeys: []
+        };
+        this.linkTo = this.linkTo.bind(this)
+    }
+
+    // componentWillReceiveProps() {
+    //     this.setState({selectedKeys: [this.props.router.location.pathname]});
+    // }
+
+    // componentDidMount() {
+    //     this.setState({selectedKeys: [this.props.router.location.pathname]});
+    // }
+
+    linkTo(item) {
+        this.props.go(item.key);
+    }
+
+    render() {
+        return (
+            <Menu theme="dark" mode={"inline"}
+                  defaultSelectedKeys={['menuA']}
+                  selectedKeys={this.state.selectedKeys}
+                  onClick={this.linkTo}
+            >
+                <Menu.Item key="/menuA">
+                    <Icon type="user"/>
+                    <span className="nav-text">
+                        menuA
+                    </span>
+                </Menu.Item>
+                <SubMenu key="/sub1" title={<span><Icon type="mail"/><span>Navigation One</span></span>}>
+                    <Menu.Item key="1">Option 1</Menu.Item>
+                    <Menu.Item key="2">Option 2</Menu.Item>
+                </SubMenu>
+            </Menu>
+        );
+    }
+}
+
+// Menus.PropTypes = {
+//     go: PropTypes.func.isRequired,
+//     router: PropTypes.func.isRequired
+// };
+
+export default Menus;
