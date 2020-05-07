c65771f56f0f41e2150d7ce1673c199d89f750e3
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-06 23:02:52 -0500

    test link work but need to refactor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import {
   LaptopOutlined,
-  NotificationOutlined,
   HomeOutlined,
 } from "@ant-design/icons";
 import SearchForm from "./MenuAList/SearchForm";
@@ -21,7 +22,6 @@ import  Breadcrumb from 'antd/es/breadcrumb';
 import Tabs from 'antd/es/tabs';
 import { Cricketer, ODICareer, Batting, Bowling, TestCareer } from "./Cricketer";
 import CareerDetails from './CareerDetails';
-// import useFetchData from './service';
 import Todo from './todo'
 import Modal from 'antd/es/modal';
 const { TabPane } = Tabs;
@@ -59,153 +59,160 @@ function App() {
     });
   };
   return (
-    <div className="App">
-      <Layout>
-        <Header style={{ background: "white" }}>ant-react-redux</Header>
-      </Layout>
-
-      <Layout style={{ padding: "10px" }}>
-        <Sider width={200}>
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={["1"]}
-            defaultOpenKeys={["sub1"]}
-            style={{ height: "100%", borderRight: 0 }}
-          >
-            <SubMenu
-              key="sub1"
-              icon={<UserOutlined />}
-              title="Menu A"
-            ></SubMenu>
-            <SubMenu
-              key="sub2"
-              icon={<LaptopOutlined />}
-              title="Navigation One"
-            >
-              <Menu.Item key="5">option 1</Menu.Item>
-              <Menu.Item key="6">option 2</Menu.Item>
-            </SubMenu>
-          </Menu>
-        </Sider>
-        <Layout
-          style={{
-            padding: "10px 10px 24px 44px",
-            margin: { top: 0, right: 0, bottom: 0, left: 10 },
-          }}
-        >
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>
-              <HomeOutlined />
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>
-              <UserOutlined />
-              Menu A
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-          </Breadcrumb>
-          <Content
-            className="site-layout-background"
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-            }}
-          >
-            <Title level={4}>Menu A</Title>
-  
-            <SearchForm />
-            {/* <SearchResult /> */}
-          </Content>
-        </Layout>
-      </Layout>
-
-
-
-
-
-
-
-      <Layout>
-        <Header style={{ background: "green", padding: "10px" }}>
-          <Avatar style={{ float: "right" }} icon={<UserOutlined />} />
-
-          <Title style={{ color: "white" }} level={3}>
-            Brand
-          </Title>
-        </Header>
-        <Tabs defaultActiveKey="1" style={{ float: "right" }}>
-          <TabPane tab="Tab 1" key="1">
-            Content of Tab Pane 1
-          </TabPane>
-          <TabPane tab="Tab 2" key="2">
-            Content of Tab Pane 2
-          </TabPane>
-          <TabPane tab="Tab 3" key="3">
-            Content of Tab Pane 3
-          </TabPane>
-        </Tabs>
+    <Router>
+      <div className="App">
         <Layout>
-          <Sider style={{ background: "red" }}>
-            <Menu defaultSelectedKeys={["Dashboard"]} mode="inline">
-              <Menu.Item key="Dashboard">Dashboard</Menu.Item>
+          <Header style={{ background: "white" }}>ant-react-redux</Header>
+        </Layout>
+
+        <Layout style={{ padding: "10px" }}>
+          <Sider width={200}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={["1"]}
+              defaultOpenKeys={["sub1"]}
+              style={{ height: "100%", borderRight: 0 }}
+            >
               <SubMenu
-                title={
-                  <span>
-                    <MailOutlined />
-                    <span>About Us</span>
-                  </span>
-                }
+                key="sub1"
+                icon={<UserOutlined />}
+                title={<Link to="/menuA">Menu A</Link>}
+              ></SubMenu>
+              <SubMenu
+                key="sub2"
+                icon={<LaptopOutlined />}
+                title="Navigation One"
               >
-                <Menu.ItemGroup key="AboutUs" title="Country">
-                  <Menu.Item>America</Menu.Item>
-                  <Menu.Item>England</Menu.Item>
-                </Menu.ItemGroup>
+                <Menu.Item key="5">option 1</Menu.Item>
+                <Menu.Item key="6">option 2</Menu.Item>
               </SubMenu>
+              <Switch>
+                <Route path="/menuA">
+                  <App />
+                </Route>
+
+              </Switch>
             </Menu>
           </Sider>
-          <Layout>
-            <Content style={{ padding: "0 50px" }}>
-              <Breadcrumb style={{ margin: "16px 0" }}>
-                <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
-              </Breadcrumb>
-              <div style={{ background: "#fff", padding: 24, minHeight: 480 }}>
-                {/* <Batsmen name="Smith" team="AUS" runs="6973" />
-                <Bowler name="Pat" team="AUS" wickets="123" /> */}
-                <Cricketer name="Virat Kholi" team="IND" avatarSrc="./cat.png">
-                  <ODICareer matches="239">
-                    <Batting runs="11,520" score="183" />
-                    <br></br>
-                    <Bowling wickets="4" bowlingAvg="166.25" />
-                  </ODICareer>
-                  <TestCareer matches="79">
-                    <Batting runs="6,749" score="243" />
-                  </TestCareer>
-                  <ViewProfileButton name="Virat Kholi" />
-                </Cricketer>
-                <Cricketer name="J Bumrah" team="IND" avatarSrc="./bb.jpg">
-                  <TestCareer matches="12">
-                    <Bowling wickets="62" bowlingAvg="20.63" />
-                  </TestCareer>
-                  <ViewProfileButton name="J Bumrah" />
-                </Cricketer>
-              </div>
+          <Layout
+            style={{
+              padding: "10px 10px 24px 44px",
+              margin: { top: 0, right: 0, bottom: 0, left: 10 },
+            }}
+          >
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>
+                <HomeOutlined />
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <UserOutlined />
+                Menu A
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+              }}
+            >
+              <Title level={4}>Menu A</Title>
+              <SearchForm />
+              {/* <SearchResult /> */}
             </Content>
-            <CareerDetails
-              player={selectedPlayer}
-              visible={visible}
-              onClose={onClose}
-            />
-            <Footer style={{ textAlign: "center" }}>
-              Ant Design ©2018 Created by practice
-            </Footer>
           </Layout>
         </Layout>
-      </Layout>
-      <Button onClick={onModal} type="primary">
-        Modal Button
-      </Button>
-      <Todo />
-    </div>
+
+        <Layout>
+          <Header style={{ background: "green", padding: "10px" }}>
+            <Avatar style={{ float: "right" }} icon={<UserOutlined />} />
+
+            <Title style={{ color: "white" }} level={3}>
+              Brand
+            </Title>
+          </Header>
+          <Tabs defaultActiveKey="1" style={{ float: "right" }}>
+            <TabPane tab="Tab 1" key="1">
+              Content of Tab Pane 1
+            </TabPane>
+            <TabPane tab="Tab 2" key="2">
+              Content of Tab Pane 2
+            </TabPane>
+            <TabPane tab="Tab 3" key="3">
+              Content of Tab Pane 3
+            </TabPane>
+          </Tabs>
+          <Layout>
+            <Sider style={{ background: "red" }}>
+              <Menu defaultSelectedKeys={["Dashboard"]} mode="inline">
+                <Menu.Item key="Dashboard">Dashboard</Menu.Item>
+                <SubMenu
+                  title={
+                    <span>
+                      <MailOutlined />
+                      <span>About Us</span>
+                    </span>
+                  }
+                >
+                  <Menu.ItemGroup key="AboutUs" title="Country">
+                    <Menu.Item>America</Menu.Item>
+                    <Menu.Item>England</Menu.Item>
+                  </Menu.ItemGroup>
+                </SubMenu>
+              </Menu>
+            </Sider>
+            <Layout>
+              <Content style={{ padding: "0 50px" }}>
+                <Breadcrumb style={{ margin: "16px 0" }}>
+                  <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+                </Breadcrumb>
+                <div
+                  style={{ background: "#fff", padding: 24, minHeight: 480 }}
+                >
+                  {/* <Batsmen name="Smith" team="AUS" runs="6973" />
+                <Bowler name="Pat" team="AUS" wickets="123" /> */}
+                  <Cricketer
+                    name="Virat Kholi"
+                    team="IND"
+                    avatarSrc="./cat.png"
+                  >
+                    <ODICareer matches="239">
+                      <Batting runs="11,520" score="183" />
+                      <br></br>
+                      <Bowling wickets="4" bowlingAvg="166.25" />
+                    </ODICareer>
+                    <TestCareer matches="79">
+                      <Batting runs="6,749" score="243" />
+                    </TestCareer>
+                    <ViewProfileButton name="Virat Kholi" />
+                  </Cricketer>
+                  <Cricketer name="J Bumrah" team="IND" avatarSrc="./bb.jpg">
+                    <TestCareer matches="12">
+                      <Bowling wickets="62" bowlingAvg="20.63" />
+                    </TestCareer>
+                    <ViewProfileButton name="J Bumrah" />
+                  </Cricketer>
+                </div>
+              </Content>
+              <CareerDetails
+                player={selectedPlayer}
+                visible={visible}
+                onClose={onClose}
+              />
+              <Footer style={{ textAlign: "center" }}>
+                Ant Design ©2018 Created by practice
+              </Footer>
+            </Layout>
+          </Layout>
+        </Layout>
+        <Button onClick={onModal} type="primary">
+          Modal Button
+        </Button>
+        <Todo />
+      </div>
+    </Router>
   );
 }
 
